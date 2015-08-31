0027004f83f4c266b13b01ca47d6e9f842feb3c1
SHI FU CHEN <shifuchen@me.com>
2015-08-31 11:06:12 +0800

    Initial commit

---
+++ app.js
@@ -0,0 +1,12 @@
+var main = function() {
+  "use strict";
+  document.getElementById("searchbox").focus();
+  $('.word').click(function() {
+    var txt = $(this).text().replace(/\s/g, "");
+    $('.word').removeClass('current');
+    $(this).addClass('current');
+    $('.content').html('<iframe height="100%" width="100%" frameborder="0" src="contents/' + txt + '.html"></iframe>');
+  });
+};
+
+$(document).ready(main);
